db7896a60998a0d76af648dafd6aa1f6644af26e
Karl Johansson <karl.johansson@t2data.com>
2021-01-24 20:00:28 +0100

    Add food rating dialog

---
+++ lib/rate_food_dialog.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class RateFoodDialog extends StatefulWidget {
+  final Function(int rating) onRatingSet;
+
+  RateFoodDialog({
+    Key key,
+    this.onRatingSet,
+  }) : super(key: key);
+
+
+  @override
+  _RateFoodDialogState createState() => _RateFoodDialogState();
+}
+
+class _RateFoodDialogState extends State<RateFoodDialog> {
+  double rating = -1.0;
+
+  void _setTempRating(rating) {
+    setState(() {
+        this.rating = rating;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RatingBar.builder(
+          initialRating: 1.0,
+          minRating: 1.0,
+          direction: Axis.horizontal,
+          allowHalfRating: false,
+          itemCount: 5,
+          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+          itemBuilder: (context, _) => Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+          onRatingUpdate: (rating) {
+            _setTempRating(rating);
+          },
+        ),
+        rating > 0.0 ? ElevatedButton(
+          onPressed: () {
+            if(rating > 0.0) {
+              widget.onRatingSet(rating.round());
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+          ),
+          child: const Text(
+            'Continue',
+            style: TextStyle(fontSize: 20)
+          ),
+        ) : Container()
+      ],
+    );
+  }
+}
---
+++ test/rate_food_dialog_test.dart
@@ -0,0 +1,28 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:happynomnom/rate_food_dialog.dart';
+
+void main() {
+  testWidgets('Rate food dialog ', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: RateFoodDialog()
+        )
+      );
+
+      expect(find.text('Continue'), findsNothing);
+
+      await tester.tap(find.byIcon(Icons.favorite).first);
+      await tester.pump();
+
+      expect(find.text('Continue'), findsOneWidget);
+  });
+}
